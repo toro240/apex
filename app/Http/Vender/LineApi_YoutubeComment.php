3d3f2bf3ffb3f1f305cace78500398fbaf2d60cb
toro240 <toron240@gmail.com>
2021-10-10 17:54:17 +0900

    add log

--- app/Http/Vender/LineApi_YoutubeComment.php
+++ app/Http/Vender/LineApi_YoutubeComment.php
@@ -4,6 +4,7 @@ namespace App\Http\Vender;
 
 use App\Http\Vender\LineApi;
 use LINE\LINEBot;
+use Illuminate\Support\Facades\Log;
 
 class LineApi_YoutubeComment extends LineApi
 {
@@ -15,10 +16,14 @@ class LineApi_YoutubeComment extends LineApi
           continue;
         }
 
+        Log::info($event);
+
         $message = $event->getText();
         if(!str_starts_with($message, '/コメント')) {
           continue;
         }
+
+
 
         $this->lineBot->replyText($reply_token, 'hoge');
       }
