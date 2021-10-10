cf61ed259157530f7f9a54a5c0fff7d542a31501
toro240 <toron240@gmail.com>
2021-10-10 16:43:21 +0900

    changed webhook routing

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('test','Api\TestController@index');
+Route::post('test','Api\TestController@index');
