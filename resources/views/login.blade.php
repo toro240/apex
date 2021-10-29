86cf1060a2d056e95785b718971c29b03d999872
toro240 <toron240@gmail.com>
2021-10-30 03:58:34 +0900

    Login 画面作成

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>
+            Login
+        </title>
+    </head>
+    <body>
+        <h1>{{ $title }}</h1>
+    </body>
+</html>
