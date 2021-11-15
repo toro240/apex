238437d326f62d40fa807927d1510ca7d01a5ccd
toro240 <toron240@gmail.com>
2021-11-15 14:18:39 +0900

    task's limited_at display week

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,7 +60,7 @@
                                     Limited At
                                 </div>
                                 <div class="col-md-6">
-                                    {{ $task->limited_at->format('Y-m-d') }}
+                                    {{ $task->limited_at->isoFormat('Y年M月d日(ddd)') }}
                                 </div>
                             </div>
                             <hr />
