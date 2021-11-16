197644e907c309dac1a3082f4d0a0800234e29ac
toro240 <toron240@gmail.com>
2021-11-16 11:43:34 +0900

    remove root path

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
 });
 
 Route::get('/login', 'LoginController@index')->name('login');
